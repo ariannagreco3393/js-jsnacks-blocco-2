6f939c844a34da5a6142dbf5742a45baf4b13964
ariannagreco3393 <a.greco3393@gmail.com>
2022-03-31 14:14:39 +0200

    add snack 2

---
+++ snack-2/main.js
@@ -0,0 +1,10 @@
+/* Inserisci un numero, se è pari stampa il numero,
+se è dispari stampa il numero successivo. */
+
+let userNumber = parseInt(prompt('inserisci un numero'))
+
+if (userNumber % 2 == 0) {
+    console.log(userNumber);
+} else {
+    console.log(userNumber + 1);
+}
